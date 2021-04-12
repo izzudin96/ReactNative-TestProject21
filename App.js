7d25cac53c1fb2c9f057e0f8cd861b32042f3a85
Izzudin Anuar <izzudinanuar96@gmail.com>
2021-04-12 16:29:26 +1200

    wip

--- App.js
+++ App.js
@@ -1,23 +1,10 @@
 import React from 'react';
 import { StyleSheet, SafeAreaView, Alert, Button } from 'react-native';
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-
-  const handlePress = () => {
-    Alert.alert("My title", "My message", [
-        {text: "Yes"},
-        {text: "No"},
-        {text: "Not sure"},
-        {text: "Interesting", onPress: function() { console.log('interesting!!!!') }}
-    ]);
-
-    Alert.prompt('Hello', 'Hi there', (text) => { console.log(text)});
-  }
-
   return (
-    <SafeAreaView style={styles.container}>
-        <Button title="Click Me" onPress={handlePress} color="orange"/>
-    </SafeAreaView>
+    <WelcomeScreen/>
   );
 }
 
